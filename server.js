6d5f15685829b0d27bd6872cef2ef15f324c5316
Gumbo64 <chickennuggetcrazykid@gmail.com>
2020-10-10 18:10:10 +1100

    purged shit q table

--- server.js
+++ server.js
@@ -22,95 +22,7 @@ const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 // const { exception } = require('console');
 
 
-var env,lv_state=[],lv_action,lv_reward,lv_score=0,lv_init='X',f=0,speed=1;
-try {
-  let rawdata = fs.readFileSync('QTable.json');
-  Q_table = JSON.parse(rawdata);
-  // console.log(Q_table)
-  
-} catch (error) {
-  console.log('Creating QTable.json')
-  Q_table = {}
-}
 
-gamma = 1;// Future discount
-alpha = 0.7;//Learning rate 0.1
-
-
-randprob = 0.1;
-tryharddiscount = 1;
-
-s = [];
-
-// actionset = 2 means you get 0 or 1
-actionSet = 2;
-
-
-
-function saveQTable(){
-    // convert JSON object to string
-  clonetable = Q_table;
-  data = JSON.stringify(clonetable, null, 4);
-
-  // write JSON string to a file
-  fs.writeFile('QTable.json', data, (err) => {
-      if (err) {
-          throw err;
-      }
-      console.log('Saved')
-      lastsave = Date.now()
-  });
-
-}
-
-
-function getQ(s, a){
-  var config = [];
-  let i = 0
-  for (i=0;i<s.length;i++){
-    config.push(s[i])
-  }
-  config.push(a)
-  // console.log(config)
-  if (!(config in Q_table)) {
-     // If there's no entry in the given Q-table for the given state-action
-     // pair, return a default reward score as 0
-     return 0;
-  }
-  return Q_table[config];
-}
-function setQ(s, a, r){
-  var config = [ s[0], s[1], a];
-      if (!(config in Q_table)) {
-        Q_table[config] = 0;
-      }
-      Q_table[config] += r;
-}
-function logreturn(a){
-  console.log(a)
-  return a
-}
-function getAction(state){
-  if(Math.random()<= randprob){
-    return Math.floor(Math.random()*actionSet)
-  }
-  var rewardForStay = getQ(state, 0);
-  var rewardForJump = getQ(state, 1);
-
-  if (rewardForStay > rewardForJump) {
-    // If reward for Stay is higher, command the flappy bird to stay
-    return 0;
-  } else if (rewardForStay < rewardForJump) {
-    // If reward for Jump is higher, command the flappy bird to jump
-    return 1;
-  } else {
-      return 1; 
-  }
-}
-function bestQ(state){
-  // console.log(state)
-  return bestset(state)[1]
-}
 mindistance = 455;
 steps=0;
 function calculatereward(tag){
@@ -154,22 +66,6 @@ function calculatereward(tag){
   
 }
 
-function implementreward(s, a,tag){
-    var rewardForState=0;
-    var futureState = getState(tag);
-    
-  
-  //What is the reward for this step
-  rewardForState = calculatereward(tag)
-  
-  var optimalFutureValue = Math.max(getQ(futureState, 0), 
-  getQ(futureState, 1));
-var updateValue = alpha*(rewardForState + gamma * optimalFutureValue - getQ(s, a));
-
-setQ(s, a, updateValue);
-}    
-
-
 
 var Engine = Matter.Engine,
     Render = Matter.Render,
@@ -185,73 +81,6 @@ nunjucks.configure( '.', {
     express: app
 });
 
-const modelFitConfig = {              // Exactly the same idea here by using tfjs's model's
-    epochs: 1,                        // fit config.
-    stepsPerEpoch: 16
-};
-
-function getState(tag){
-    let AIship = ships[tag];
-    let distance = Math.hypot(
-        Math.abs(AIship.position.x-1000),
-        Math.abs(AIship.position.y-1000))
-    let ShipToPlanetAngle = Math.atan2(
-        Math.abs(AIship.position.x-1000),
-        Math.abs(AIship.position.y-1000))
-    let shipangle = AIship.angle % (2*Math.PI)
-    // let angledifference = ShipToPlanetAngle - shipangle;
-
-    distance = Math.round(distance/20)*20;
-    ShipToPlanetAngle = Math.round(ShipToPlanetAngle*30)/30
-      
-    
-    return [distance,ShipToPlanetAngle]
-    // console.log('state')
-    // return logreturn(Math.floor(Math.random() * (actionSet)))
-}
-function implementAction(action){
-  
-  //actions = ships[z].input;
-  // left = actions[0];
-  // right=actions[1];
-  // up=actions[2];
-  // down=actions[3];
-  // shoot=actions[4];
-  switch(action){
-    case 0:
-      ships['AI'].input = [false,true,true,false,false]
-      break;
-    case 1:
-      ships['AI'].input = [true,false,true,false,false]
-      break;
-  
-    case 2:
-      ships['AI'].input = [false,false,true,false,false]
-      break;
-    case 3:
-      ships['AI'].input = [true,false,false,false,false]
-      break;
-    case 4:
-      ships['AI'].input = [false,true,false,false,false]
-      break;
-  
-    case 5:
-      ships['AI'].input = [false,true,false,false,false]
-      break;
-    case 6:
-      ships['AI'].input = [true,false,false,true,false]
-      break;
-  
-    case 7:
-      ships['AI'].input = [false,true,false,true,false]
-      break;
-    case 8:
-      ships['AI'].input = [false,false,false,true,false]
-      break;
-  }
-
-}
-
 inputs = {};
 ships = {};
 manualplanets = [{'x':1000,'y':1000,'r':500}];
@@ -265,11 +94,7 @@ for (i=0;i<manualplanets.length;i++){
   World.add(engine.world, engineplanets[i]);
 }
 
-
-ships['AI'] = Bodies.rectangle(400, 200, 80, 80,{'frictionAir':0, 'colour':'AI','username':'OrbitAI','input':[false,false,false,false,false],'structure':{"0":{"0":'bullet'}}});
 console.log('OrbitAI',' joined');
-World.add(engine.world, [ships['AI']]);
-
 
 app.set('view engine', 'nunjucks')
 app.use('/static', express.static('static'))
@@ -322,26 +147,7 @@ app.listen(port, function(){
 
 
 
-actionhistory = {}
-
-
-function actioncount(action){
-  if(!actionhistory[action]){
-    actionhistory[action]=1
-  }else{
-    actionhistory[action]+=1
-  }
-}
-
-starttime = Date.now();
-lastsave = Date.now();
-nowtag='AI'
-lv_state   = getState(nowtag);
-lv_action = getAction(lv_state,nowtag); // s is an array of length 3
 setInterval(intervalloop, tickrate);
-setInterval(saveQTable, 600000);
-// setInterval(saveQTable, 600);
-
 
 
 function intervalloop(){
@@ -353,12 +159,6 @@ function intervalloop(){
   io.emit('states',parsedships());
   io.emit('planets',parsedplanets());
 
-  implementreward(lv_state,lv_action,nowtag);
-  lv_state   = getState(nowtag);
-  lv_action = getAction(lv_state);
-
-  actioncount(lv_action)
-  implementAction(lv_action)
 }
 
 function parsedships(){
